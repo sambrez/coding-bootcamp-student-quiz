4eb252b9f71444301a8be90ab287f292647ee2d6
Sam Brez <sambreslin02@gmail.com>
2023-10-23 21:24:28 -0400

    array of questions in js

---
+++ assets/script.js
@@ -0,0 +1,53 @@
+// variable that establishes the quizzes set of questions
+const questions = [
+    {
+        question: "",
+        multipleChoice:["", "", "", ""],
+        answer: ""
+    },
+    {
+        question: "",
+        multipleChoice:["", "", "", ""],
+        answer: ""
+    },
+    {
+        question: "",
+        multipleChoice:["", "", "", ""],
+        answer: ""
+    },
+    {
+        question: "",
+        multipleChoice:["", "", "", ""],
+        answer: ""
+    },
+    {
+        question: "",
+        multipleChoice:["", "", "", ""],
+        answer: ""
+    },
+    {
+        question: "",
+        multipleChoice:["", "", "", ""],
+        answer: ""
+    },
+    {
+        question: "",
+        multipleChoice:["", "", "", ""],
+        answer: ""
+    },
+    {
+        question: "",
+        multipleChoice:["", "", "", ""],
+        answer: ""
+    },
+    {
+        question: "",
+        multipleChoice:["", "", "", ""],
+        answer: ""
+    },
+    {
+        question: "",
+        multipleChoice:["", "", "", ""],
+        answer: ""
+    },
+];
