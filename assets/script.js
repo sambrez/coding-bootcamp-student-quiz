19f74052c7db9e1f1c060aaaca1536ff278a34b5
Sam Brez <sambreslin02@gmail.com>
2023-10-27 20:55:58 -0400

    finalized questions

--- assets/script.js
+++ assets/script.js
@@ -1,53 +1,53 @@
 // variable that establishes the quizzes set of questions
 const questions = [
     {
-        question: "",
-        multipleChoice:["", "", "", ""],
-        answer: ""
+        question: "Where in the HTML file should the JavaScript file link be located?",
+        multipleChoice:["As the first element in the <head>", "As the last element in the <head>", "As the first element in the <body>", "As the last element in the <body>"],
+        answer: "As the last element in the <body>"
     },
     {
-        question: "",
-        multipleChoice:["", "", "", ""],
-        answer: ""
+        question: "Which of these in NOT a way to declare a variable in JavaScript?",
+        multipleChoice:["const", "if", "let", "var"],
+        answer: "if"
     },
     {
-        question: "",
-        multipleChoice:["", "", "", ""],
-        answer: ""
+        question: "____ is a JavaScript datatype.",
+        multipleChoice:["string", "boolean", "number", "all of the above"],
+        answer: "all of the above"
     },
     {
-        question: "",
-        multipleChoice:["", "", "", ""],
-        answer: ""
+        question: "A JavaSCript event handler is ____.",
+        multipleChoice:["used to handle and verify user input, user actions, and browser actions", "used to assign a value to a variable", "used for storing and manipulating text", "used to perform mathematical tasks on numbers"],
+        answer: "used to handle and verify user input, user actions, and browser actions"
     },
     {
-        question: "",
-        multipleChoice:["", "", "", ""],
-        answer: ""
+        question: "'An array is a special variable, which can _______.'",
+        multipleChoice:["deal only with numbers", "deal only with strings", "hold more than one value", "evaluate true or false"],
+        answer: "hold more than one value"
     },
     {
-        question: "",
-        multipleChoice:["", "", "", ""],
-        answer: ""
+        question: "String must be written in ____.",
+        multipleChoice:["single quotes", "doubles quotes", "quotaions of either kind aren't required", "either single or double quotes"],
+        answer: "either single or double quotes"
     },
     {
-        question: "",
-        multipleChoice:["", "", "", ""],
-        answer: ""
+        question: "A function must be _____.",
+        multipleChoice:["called", "declared with the keyword 'function'", "given specific name", "all of the above"],
+        answer: "all of the above"
     },
     {
-        question: "",
-        multipleChoice:["", "", "", ""],
-        answer: ""
+        question: "Which of these is NOT a JavaScript scope?",
+        multipleChoice:["block", "function", "aside", "global"],
+        answer: "aside"
     },
     {
-        question: "",
-        multipleChoice:["", "", "", ""],
-        answer: ""
+        question: "Java and JavaScript are synonymous.",
+        multipleChoice:["true", "false"],
+        answer: "false"
     },
     {
-        question: "",
-        multipleChoice:["", "", "", ""],
-        answer: ""
+        question: "If, else statements are also known as _____.",
+        multipleChoice:["either, or statements", "variable statements", "Java statements", "conditional statements"],
+        answer: "conditional statements"
     },
 ];
